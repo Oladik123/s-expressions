5e82b83ed2483562c670c3fec994d668d1647f6f
Dmitry Ivanishkin <dimakiber9898@ya.ru>
2021-01-26 18:49:01 +0700

    fix search bugs

--- project/src/search.rb
+++ project/src/search.rb
@@ -4,26 +4,16 @@ class Element
   def search(string)
     @absolute = string[0] == '/'[0]
     queue = []
-    root = make_bi_tree(nil, self)
-    esa = extract_search_atoms(string)
+    root = make_tree(nil, self)
+    esa = Element.extract_search_atoms(string)
     queue.push(root)
     until queue.empty?
       current = queue.shift
       #compare
       result = compare_nodes(current, esa)
       #print results
-      if result
-        print_hash(current)
-=begin
-        path = ""
-        curr = current
-        begin
-          path.insert(0, '/'+curr[:key])
-          curr = curr[:parent]
-        end while curr != nil
-        puts path
-=end
-      end
+      print_hash(current) if result
+
       #add children to queue
       if current[:children]
         current[:children].each { |x| queue.push(x) }
@@ -58,79 +48,42 @@ class Element
   def compare_nodes(node, esa, index = 0)
     #check if we got end of matching string
     if index >= esa.length
-      if @absolute and node[:parent] != nil
-        return false
-      else
-        return true
-      end
+      return end_check(node, esa)
     end
 
-    #joker check
-    if esa[esa.length - 1 - index][:key] == '*'
-      current = node
-      result = [false]
-      while current[:parent] != nil
-        result << compare_nodes(current[:parent], esa, index + 1)
-        current = current[:parent]
-      end
-      if current[:parent] == nil and index == esa.length - 1
-        return true
-      end
-      return result.any?
-    else
-      #base check
-      if esa[esa.length - 1 - index][:key] != node[:key]
-        return false
-      end
+    #wildcard check
+    if esa[- 1 - index][:key] == '*'
+      return wildcard_check(node, esa, index)
+    end
+
+    #base check
+    if esa[- 1 - index][:key] != node[:key]
+      return false
     end
 
     #params check
-    if esa[esa.length - 1 - index][:params] != nil
-      esa[esa.length - 1 - index][:params].each { |x|
-        key = x[:key]
-        value = x[:value]
-        if value == nil
-          if node[:value] != key
-            return false
-          end
-        else
-          if node[:params] == nil
-            return false
-          end
-          found = false
-          node[:params].each { |y|
-            if y[:key] == key
-              if y[:value].to_s == value
-                found = true
-              end
-            end
-          }
-          unless found
-            return false
-          end
-        end
-      }
+    if esa[- 1 - index][:params] != nil
+      return false unless params_matched(node, esa, index)
     end
 
-    #check if we got top of tree
+    # upper
     if node[:parent] != nil
-      compare_nodes(node[:parent], esa, index + 1)
-    else
-      if index == esa.length - 1
-        return true
-      else
-        return false
-      end
+      return compare_nodes(node[:parent], esa, index + 1)
     end
 
-    true
+    # check root reached
+    if index == esa.length - 1
+      return true
+    else
+      return false
+    end
   end
 
-  def make_bi_tree(parent, element)
+  def make_tree(parent, element)
     params = element.get_params
     if params
-      params = params.map { |ele|
-        param = {:key => ele.get_key.to_s, :value => ele.get_data}
+      params = params.map { |elem|
+        param = {:key => elem.get_key.to_s, :value => elem.get_data}
         param
       }
     end
@@ -145,25 +98,21 @@ class Element
         :children => nil
     }
     if children
-      children = children.map { |x| make_bi_tree(data, x) }
+      children = children.map { |x| make_tree(data, x) }
     end
     data[:children] = children
-
     data
   end
 
-  def extract_search_atoms (string)
+  def self.extract_search_atoms (string)
     string_literals = []
     string.gsub(/[\w\[=\]*"]+/) { |x| string_literals << x }
     string_literals.map { |str|
-      tok = str.match(/(\w+|\*)/)
-      tok = tok.to_a
-      _, key = tok
+      key = str.match(/(\w+|\*)/)[0]
       tok = str.scan(/\[(\w+)=(\w+)\]/)
       params = tok.map { |t|
         paramkey, paramvalue = t
-        param = {:key => paramkey, :value => paramvalue}
-        param
+        {:key => paramkey, :value => paramvalue}
       }
       tok = str.scan(/\[("\w+")\]/)
       unless tok.empty?
@@ -188,7 +137,7 @@ class Element
       end
     elsif result.kind_of?(Element)
       return nil if (result.get_key != '@'.to_sym)
-      result.get_data
+      [result.get_data]
     else
       nil
     end
@@ -223,6 +172,60 @@ class Element
 
   def get_data
     @data
+  end
+
+  private
+
+  def params_matched(node, esa, index)
+    matched = true
+    esa[-1 - index][:params].each { |x|
+      key = x[:key]
+      value = x[:value]
+      if value == nil
+        if node[:value] != key
+          matched = false
+          break
+        end
+      elsif node[:params] == nil
+        matched = false
+        break
+      else
+        found = false
+        node[:params].each { |y|
+          if y[:key] == key
+            if y[:value].to_s == value
+              found = true
+            end
+          end
+        }
+        unless found
+          matched = false
+          break
+        end
+      end
+    }
+    matched
+  end
+
+  def wildcard_check(node, esa, index)
+    current = node
+    result = [false]
+    while current[:parent] != nil
+      result << compare_nodes(current[:parent], esa, index + 1)
+      current = current[:parent]
+    end
+    if current[:parent] == nil and index == esa.length - 1
+      return true
+    end
+    result.any?
+  end
+
+  def end_check(node, esa)
+    if @absolute and node[:parent] != nil
+      false
+    else
+      true
+    end
   end
 
 end
--- project/tests/search_test.rb
+++ project/tests/search_test.rb
@@ -25,18 +25,43 @@ class SearchTest < Test::Unit::TestCase
   def test_compare_advanced
     parser = Parser.new
     example1 = parser.parse('(fruits "hello")')
-    root = example1.make_bi_tree(nil, example1)
-    esa = example1.extract_search_atoms('/fruits')
-    assert_equal(true, example1.compare_nodes(root, esa))
+    root = example1.make_tree(nil, example1)
+    esa = Element.extract_search_atoms('/fruits')
+    assert_true(example1.compare_nodes(root, esa))
 
-    esa = example1.extract_search_atoms('/fruits["hello"]')
-    assert_equal(true, example1.compare_nodes(root, esa))
+    esa = Element.extract_search_atoms('/fruits["hello"]')
+    assert_true(example1.compare_nodes(root, esa))
 
-    esa = example1.extract_search_atoms('/*')
-    assert_equal(true, example1.compare_nodes(root, esa))
+    esa = Element.extract_search_atoms('/*')
+    assert_true(example1.compare_nodes(root, esa))
 
-    esa = example1.extract_search_atoms('fruits')
-    assert_equal(true, example1.compare_nodes(root, esa))
+    esa = Element.extract_search_atoms('*')
+    assert_true(example1.compare_nodes(root, esa))
+
+    esa = Element.extract_search_atoms('fruits')
+    assert_true(example1.compare_nodes(root, esa))
+  end
+
+  def test_compare_params
+    parser = Parser.new
+    example_5 = parser.parse("(fruit (@ (weight 5))")
+    example_1 = parser.parse("(fruit (@ (weight 1))")
+    root_5 = example_5.make_tree(nil, example_5)
+    root_1 = example_1.make_tree(nil, example_1)
+
+    esa_5 = Element.extract_search_atoms('fruit[weight=5]')
+    esa_1 = Element.extract_search_atoms('fruit[weight=1]')
+    esa_raw = Element.extract_search_atoms('fruit')
+
+    assert_true(example_5.compare_nodes(root_5, esa_5))
+    assert_false(example_1.compare_nodes(root_1, esa_5))
+
+    assert_true(example_1.compare_nodes(root_1, esa_1))
+    assert_false(example_5.compare_nodes(root_5, esa_1))
+
+    assert_true(example_1.compare_nodes(root_1, esa_raw))
+    assert_true(example_5.compare_nodes(root_5, esa_raw))
+
   end
 
 end
